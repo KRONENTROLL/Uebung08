5d771ed6b39c5c42ddbf6021476d94e0697bab11
Jan Buntenbroich <jan@buntenbroich.de>
2025-12-11 18:44:37 +0100

    h1 fertig

--- src/h1/Bruch.java
+++ src/h1/Bruch.java
@@ -1,10 +1,61 @@
 package h1;
 
 public class Bruch {
-	int zaehler;
-	int nenner;
-//	ggT
-	private int ggT (int x, int y) {
-		return 4;
-	}
+    private int zaehler;
+    private int nenner;
+
+    public Bruch(int zaehler, int nenner) {
+        if (nenner == 0) {
+            throw new IllegalArgumentException("Nenner darf nicht 0 sein.");
+        }
+        this.zaehler = zaehler;
+        this.nenner = nenner;
+    }
+
+    private int ggT(int x, int y) {
+        int gZahl = 0;
+        int kZahl = 0;
+        if (x > y) {
+            gZahl = x;
+            kZahl = y;
+        }
+        if (x < y) {
+            gZahl = y;
+            kZahl = x;
+        }
+        if (x == y) {
+            gZahl = x;
+            kZahl = y;
+        }
+        if (kZahl == 0) {
+            return gZahl;
+        }
+
+        int rest = 1;
+        while (rest > 0) {
+            rest = gZahl % kZahl;
+            gZahl = kZahl;
+            kZahl = rest;
+        }
+        rest = gZahl;
+        return rest;
+    }
+
+    public void shorten() {
+        int teiler = ggT(this.zaehler, this.nenner);
+        this.zaehler /= teiler;
+        this.nenner /= teiler;
+    }
+
+    public boolean hasSameValueAs(Bruch b) {
+        return this.zaehler * b.nenner == this.nenner * b.zaehler;
+    }
+
+    public int getZaehler() {
+        return zaehler;
+    }
+
+    public int getNenner() {
+        return nenner;
+    }
 }
--- src/h1/H1_main.java
+++ src/h1/H1_main.java
@@ -1,7 +1,20 @@
 package h1;
 
 public class H1_main {
-	public static void main(String[] args) {
-		
-	}
+    public static void main(String[] args) {
+
+        Bruch b1 = new Bruch(25, 5);
+        b1.shorten();
+        System.out.println(b1.getZaehler() + "/" + b1.getNenner()); // 5/1
+
+        Bruch b2 = new Bruch(8, 12);
+        b2.shorten();
+        System.out.println(b2.getZaehler() + "/" + b2.getNenner()); // 2/3
+
+        Bruch b3 = new Bruch(5, 3);
+        Bruch b4 = new Bruch(15, 9);
+        System.out.println(b3.hasSameValueAs(b4)); // true
+    }
 }
+
+
